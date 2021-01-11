102a4a10f428122a66f3187782c2b36381fac6f9
Chris Tomasello <ctom96@vt.edu>
2021-01-11 00:02:01 -0500

    Added skills stuff

--- mythchar/skills.go
+++ mythchar/skills.go
@@ -1,8 +1,27 @@
 package mythchar
 
-
-
 // All skills look the same
 type Skill struct {
+	Name  string
+	Base1 Stat
+	Base2 Stat
+}
 
+// Skills for a specific character
+type Skills struct {
+	Athletics Skill,
+	Boating Skill,
+	Brawn Skill,
+	Conceal Skill,
+	Customs Skill,
+	Dance Skill,
+	Deceit Skill,
+	Drive Skill,
+	Endurance Skill,
+	Evade Skill,
+	FirstAid Skill,
+	Influence Skill,
+	Insight Skill,
+	Locale Skill,
+	NativeTongue Skill,
 }
